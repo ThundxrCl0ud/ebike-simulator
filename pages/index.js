4ef1713b6c3b7b907b8b128fb7f31fcc896f02d6
ThundxrCl0ud <lightningedits0723@gmail.com>
2025-06-07 12:25:38 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -290,7 +290,7 @@ export default function EBikeCustomizer() {
   const filteredColors = useMemo(() => {
     if (activeCategory !== "frameColor") return [];
 
-    let colors = parts.FrameColor?.[colorCategory] || [];
+ let colors = Object.keys(parts.frameColor?.[colorCategory] || {});
     if (searchTerm.trim() !== "") {
       const term = searchTerm.toLowerCase();
       colors = colors.filter((c) => c.toLowerCase().includes(term));
